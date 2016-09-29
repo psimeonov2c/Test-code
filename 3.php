2154480f816d6b82cb2c3185a14c84f339918e94
psimeonov2c <psimeonov.2create@gmail.com>
2016-09-29 15:42:17 +0300

    Add files via upload

--- 3.php
+++ 3.php
@@ -3,22 +3,19 @@
 <script>
 	// При натискане на линка:
 	//  - покажете диалогов прозореж на потребителя по този начин: window.confirm("You're about to leave this page. Are you sure? ");
-	$( document ).ready(function(){
-		$( "#external-link" ).on( "click", function(event){
-			event.preventDefault();
-			if( window.confirm( "You're about to leave this page. Are you sure? ") ){
-				console.log('You clicked OK');
-			}else{
-				console.log('You clicked Cancel');
-			}
-		});
-	});
-	
-	
-	
 	//  - повече информация за window.confirm() можете да прочетете тук: https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm
 	//  - ако потребителя натисне "Cancel" в доалоговия прозорец, предотвратете навигирането до външната страница
 	//  - ако потребителя натисне "OK", не правете нищо, и позолете навигирането до външната страница
+	$( document ).ready(function(){
+		$( "#external-link" ).on( "click", function(event){
+			if( window.confirm( "You're about to leave this page. Are you sure? ") ){
+				console.log('You clicked OK - you now follow external link');
+			}else{
+				console.log('You clicked Cancel - not going anywhere');
+				event.preventDefault();
+			}
+		});
+	});
 </script>
 
 <a href="http://example.com/" id="external-link">Go to google.com</a>
